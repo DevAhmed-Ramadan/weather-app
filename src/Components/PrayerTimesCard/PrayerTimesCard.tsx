b52fce7a8090c1d1c1541d6d31608f28ca7c5d97
DevAhmed-Ramadan <ahmed.ramadan.afifi98@gmail.com>
2024-07-30 21:03:48 +0300

    prayer times commit

--- src/Components/PrayerTimesCard/PrayerTimesCard.tsx
+++ src/Components/PrayerTimesCard/PrayerTimesCard.tsx
@@ -12,14 +12,14 @@ const PrayerTimesCard: React.FC<PrayerTimesCardProps> = ({ data }) => {
     }
 
     const prayerTimes = data.data.timings;
-    const filteredArray = Object.entries(prayerTimes).slice(0, 5); // Adjust the slice if needed
+    const filteredArray = Object.entries(prayerTimes).slice(0, 7); // Adjust the slice if needed
 
     return (
         <div className=' dark:text-white dark:bg-slate-800 flex flex-col bg-white text-[#1E1E1E] h-full rounded-[1rem] px-3 shadow-xl'>
             <h2 className='text-[25px] font-bold mt-1 mb-1'>Prayer Times</h2>
             <div className='mt-2'>
                 {filteredArray.map(([prayer, time]) => (
-                    <div key={prayer} className='flex justify-between py-1 mb-8 text-xl'>
+                    <div key={prayer} className='flex justify-between py-1 mb-4 text-xl'>
                         <span>{prayer}</span>
                         <span>{time}</span>
                     </div>
